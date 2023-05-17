6363bcb587a27d2b0d25a73ffb26abf4483dd497
Harry Fosu <harry23adjei@yahoo.com>
2023-05-17 12:16:27 +0200

    Finished cocktail_project

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem sequi ducimus omnis ipsum quaerat. Tempore, sapiente pariatur? Ea odio ex, voluptates vero, assumenda magni totam nostrum quia quisquam exercitationem veniam?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias explicabo inventore error blanditiis, sapiente vel eius expedita provident accusantium officia, corporis recusandae? Officiis dolor soluta quasi at minima quas enim?
+      </p>
+    </section>
   );
 };
 
